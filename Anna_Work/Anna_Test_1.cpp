9d440ada9e2dcd4c84a91b28c053870d6f398208
brandlan <brandlan@msu.edu>
2024-02-22 16:41:47 -0500

    This is for Part 1 Numbers 1 & 2

--- Anna_Work/Anna_Test_1.cpp
+++ Anna_Work/Anna_Test_1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <mpi.h>
+#include <vector>
+#include <cstring>
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
@@ -8,23 +10,44 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int iterations = 10; // Number of ping-pong iterations
-    const int message_size = 100; // Size of the message to be exchanged
+    const int iterations = 100; // Number of ping-pong iterations
 
+    // Required as ping pong communication is done between 2 processors
     if (size != 2) {
-        std::cerr << "This program must be run with exactly 2 processes" << std::endl;
+        std::cerr << "This program must be run with exactly 2 processors" << std::endl;
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    for (int i = 0; i < iterations; ++i) {
+    double start_time, end_time;
+    for (int message_size_bytes = 2; message_size_bytes <= 4096; message_size_bytes *= 2) {
+        std::vector<char> send_buffer(message_size_bytes, 'a');
+        std::vector<char> recv_buffer(message_size_bytes);
+
+        double total_elapsed_time = 0.0;
+        for (int iter = 0; iter < iterations; ++iter) {         // Running 100 iterations for each message size
+            start_time = MPI_Wtime();  // Measure start time for both processes
+
+            if (rank == 0) {
+                // Process 0 sends a message to process 1
+                MPI_Send(send_buffer.data(), message_size_bytes, MPI_BYTE, 1, 0, MPI_COMM_WORLD); // sending to processor 1
+                MPI_Recv(recv_buffer.data(), message_size_bytes, MPI_BYTE, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receiving from processor 1
+            } else {
+                // Process 1 receives a message from process 0
+                MPI_Recv(recv_buffer.data(), message_size_bytes, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // sending to processor 0
+                MPI_Send(send_buffer.data(), message_size_bytes, MPI_BYTE, 0, 0, MPI_COMM_WORLD); // receiving from processor 0
+            }
+
+            end_time = MPI_Wtime();  // Measure end time for both processes
+
+                     if (rank == 0) {
+                double elapsed_time = end_time - start_time;
+                total_elapsed_time += elapsed_time;
+            }
+        }
+
         if (rank == 0) {
-            // Process 0 sends a message to process 1
-            MPI_Send(&message_size, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-            MPI_Recv(&message_size, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        } else {
-            // Process 1 receives a message from process 0
-            MPI_Recv(&message_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(&message_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            double average_elapsed_time = total_elapsed_time / iterations;
+            std::cout << "Message size (bytes): " << message_size_bytes << ", Average elapsed time: " << average_elapsed_time << " seconds" << std::endl;
         }
     }
 
